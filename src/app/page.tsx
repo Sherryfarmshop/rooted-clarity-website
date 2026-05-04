d35fcaebd6549672e5d95261234d601d7280ce0b
Sherryfarmshop <sherryfarmshop@users.noreply.github.com>
2026-05-04 19:41:22 -0400

    restore-all-sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Hero from "@/components/Hero";
+import TaglineBanner from "@/components/TaglineBanner";
+import Problem from "@/components/Problem";
+import HowItWorks from "@/components/HowItWorks";
+import WhatMakesUsDifferent from "@/components/WhatMakesUsDifferent";
 import Approach from "@/components/Approach";
+import Doctors from "@/components/Doctors";
+import FounderStory from "@/components/FounderStory";
+import AppFeatures from "@/components/AppFeatures";
+import Supplements from "@/components/Supplements";
 import CTASignup from "@/components/CTASignup";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
@@ -10,7 +18,15 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
+        <TaglineBanner />
+        <Problem />
+        <HowItWorks />
+        <WhatMakesUsDifferent />
         <Approach />
+        <Doctors />
+        <FounderStory />
+        <AppFeatures />
+        <Supplements />
         <CTASignup />
       </main>
       <Footer />
