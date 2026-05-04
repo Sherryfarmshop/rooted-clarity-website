fe362b6af0a8783fec0ec0befe665a2dc48134b0
Sherryfarmshop <sherryfarmshop@users.noreply.github.com>
2026-05-04 19:38:20 -0400

    streamline-page-hero-approach-cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 import Hero from "@/components/Hero";
-import TaglineBanner from "@/components/TaglineBanner";
-import Problem from "@/components/Problem";
-import HowItWorks from "@/components/HowItWorks";
-import WhatMakesUsDifferent from "@/components/WhatMakesUsDifferent";
 import Approach from "@/components/Approach";
-import Doctors from "@/components/Doctors";
-import FounderStory from "@/components/FounderStory";
-import AppFeatures from "@/components/AppFeatures";
 import CTASignup from "@/components/CTASignup";
-import Supplements from "@/components/Supplements";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
@@ -18,15 +10,7 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
-        <TaglineBanner />
-        <Problem />
-        <HowItWorks />
-        <WhatMakesUsDifferent />
         <Approach />
-        <Doctors />
-        <FounderStory />
-        <AppFeatures />
-        <Supplements />
         <CTASignup />
       </main>
       <Footer />
